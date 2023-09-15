4f8ef9204f9296e3bbe995a918a7e998b1f25cda
chendong0 <40737674+chendong0@users.noreply.github.com>
2023-09-15 19:12:04 +0800

    Add files via upload

---
+++ _posts/20230915名词标记支持WordMobiEpub.py
@@ -0,0 +1,93 @@
+import thulac
+import docx2txt
+import textract
+import tkinter as tk
+from tkinter import filedialog
+import time
+
+WIDTH = 400
+HEIGHT = 400
+
+
+def analyze_text(input_file_path, output_file_path):
+    output_file = open(output_file_path, 'w', encoding='utf-8')
+    start_time = time.time()
+    thu1 = thulac.thulac()
+
+    if input_file_path.endswith('.docx'):
+        text = docx2txt.process(input_file_path)
+    elif input_file_path.endswith('.mobi') or input_file_path.endswith('.epub'):
+        text = textract.process(input_file_path)
+    elif input_file_path.endswith('.pdf'):
+        text = textract.pdftotext(input_file_path)
+    else:
+        with open(input_file_path, 'r', encoding='utf-8') as input_file:
+            text = input_file.read()
+
+    segmented_text = thu1.cut(text, text=True)
+
+    segmented_words = segmented_text.split()
+
+    marked_words = []
+
+    for word in segmented_words:
+        if word.endswith('_n'):
+            marked_words.append(word.split('_')[0] + '/n')
+        else:
+            marked_words.append(word.split('_')[0])
+
+    marked_text = ''.join(marked_words)
+    output_file.write(marked_text)
+
+    end_time = time.time()
+    print("Runtime:", end_time - start_time, "seconds")
+
+
+def browse_input_file():
+    file_path = filedialog.askopenfilename()
+    input_entry.delete(0, tk.END)
+    input_entry.insert(0, file_path)
+
+
+def browse_output_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+    output_entry.delete(0, tk.END)
+    output_entry.insert(0, file_path)
+
+
+def analyze_button_click():
+    input_file_path = input_entry.get()
+    output_file_path = output_entry.get()
+    analyze_text(input_file_path, output_file_path)
+
+
+# Create the GUI window
+root = tk.Tk()
+root.title("Text Analyzer")
+
+# 设置窗口大小
+root.geometry(f"{WIDTH}x{HEIGHT}")
+
+# Create and place GUI elements
+input_label = tk.Label(root, text="Input Text File:")
+input_label.pack()
+
+input_entry = tk.Entry(root)
+input_entry.pack()
+
+browse_input_button = tk.Button(root, text="Browse", command=browse_input_file)
+browse_input_button.pack()
+
+output_label = tk.Label(root, text="Output Text File:")
+output_label.pack()
+
+output_entry = tk.Entry(root)
+output_entry.pack()
+
+browse_output_button = tk.Button(root, text="Browse", command=browse_output_file)
+browse_output_button.pack()
+
+analyze_button = tk.Button(root, text="Analyze", command=analyze_button_click)
+analyze_button.pack()
+
+root.mainloop()
